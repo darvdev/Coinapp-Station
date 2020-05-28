d7ceffc017445cf6114850ae653f9143172c0b61
vinstudios <mamangondarvin@gmail.com>
2020-05-28 22:30:15 +0800

    add login form

---
+++ Form_login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinappStation
+{
+    public partial class Form_login : Form
+    {
+        public Form_login()
+        {
+            InitializeComponent();
+        }
+    }
+}
